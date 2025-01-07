742a6ed22c4656006af4138cf238fc70db5699fe
BenceV95 <bencev95@gmail.com>
2025-01-07 15:04:09 +0100

    ADDED: get all categories

--- AskMate/Models/Repos/ICategoriesRepo.cs
+++ AskMate/Models/Repos/ICategoriesRepo.cs
@@ -3,7 +3,7 @@
     public interface ICategoriesRepo
     {
         public bool CreateCategory(string name);
-        public bool GetAllCategories();
+        public List<Category> GetAllCategories();
         public bool UpdateCategory(int id, string name);
         public bool DeleteCategory(int id);
 
