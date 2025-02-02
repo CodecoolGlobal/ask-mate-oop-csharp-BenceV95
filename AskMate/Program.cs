a52bc1ef8fee43b1842f9d998e9b5c220b393426
BenceV95 <bencev95@gmail.com>
2025-02-02 15:27:29 +0100

    New branch, tidied up program.cs

--- AskMate/Program.cs
+++ AskMate/Program.cs
@@ -1,10 +1,6 @@
 using AskMate.Middleware;
-using AskMate.Models;
 using AskMate.Repos;
 using AskMate.Repos.Votes;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Hosting.Server;
-using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,50 +9,8 @@ string CONNECTIONSTRING = Environment.GetEnvironmentVariable("CONNECTION_STRING"
 
 builder.Services.AddSingleton<string>(sp => CONNECTIONSTRING + "ask_mate;"+ "Pooling=true;"); // db_name + pooling
 
-
-//questions
-builder.Services.AddScoped<IQuestionsRepo, QuestionsRepo>();
-
-//answers
-builder.Services.AddScoped<IAnswersRepo, AnswersRepo>();
-
-//users
-builder.Services.AddScoped<IUserRepo, UserRepo>();
-
-//categories
-builder.Services.AddScoped<ICategoriesRepo, CategoriesRepo>();
-
-//votes
-builder.Services.AddScoped<IVotesRepo, VotesRepo>();
-
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-// use cookies
-builder.Services.AddAuthentication("Cookies")
-    .AddCookie("Cookies", options =>
-    {
-        options.Cookie.Name = "UserAuthCookie"; // Name of the cookie
-        options.LoginPath = "/User/login";     // Redirect to login if unauthorized
-        options.AccessDeniedPath = "/User/denied"; // Redirect if access is denied
-        options.ExpireTimeSpan = TimeSpan.FromHours(1); // Expiration
-    });
-
-//built in cors
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend", policy =>
-    {
-        policy.WithOrigins("http://localhost:5173")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
-    });
-});
+RegisterServices();
+AddOptionsToRegisteredServices();
 
 var app = builder.Build();
 
@@ -70,8 +24,7 @@ if (app.Environment.IsDevelopment())
 //add authetication
 app.UseAuthentication();
 
-
-app.UseCorsMiddleware();
+app.UseCorsMiddleware(); // change it to use proxy eventually !!!
 
 app.UseAuthorization();
 
@@ -81,3 +34,54 @@ app.MapControllers();
 app.UseCors("AllowFrontend");
 
 app.Run();
+
+void RegisterServices()
+{
+    //questions
+    builder.Services.AddScoped<IQuestionsRepo, QuestionsRepo>();
+
+    //answers
+    builder.Services.AddScoped<IAnswersRepo, AnswersRepo>();
+
+    //users
+    builder.Services.AddScoped<IUserRepo, UserRepo>();
+
+    //categories
+    builder.Services.AddScoped<ICategoriesRepo, CategoriesRepo>();
+
+    //votes
+    builder.Services.AddScoped<IVotesRepo, VotesRepo>();
+
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+
+    // swagger
+    builder.Services.AddSwaggerGen();
+}
+
+void AddOptionsToRegisteredServices()
+{
+    // use cookies
+    builder.Services.AddAuthentication("Cookies")
+        .AddCookie("Cookies", options =>
+        {
+            options.Cookie.Name = "UserAuthCookie"; // Name of the cookie
+            options.LoginPath = "/User/login";     // Redirect to login if unauthorized
+            options.AccessDeniedPath = "/User/denied"; // Redirect if access is denied
+            options.ExpireTimeSpan = TimeSpan.FromHours(1); // Expiration
+        });
+
+    //built in cors -> change it to be a proxy !!!
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowFrontend", policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+    });
+}
