1add13e5cea9a46d6f973d195c8735c94936d5d0
Bence Veres <bencev95@gmail.com>
2025-03-26 15:39:05 +0100

    added getting a question by category id and limit on the backend

--- AskMate/Repos/Questions/IQuestionsRepo.cs
+++ AskMate/Repos/Questions/IQuestionsRepo.cs
@@ -6,6 +6,7 @@ namespace AskMate.Repos.Questions
     {
         public List<Question> GetAllQuestions();
         public Task<Question> GetQuestion(string questionID);
+        public List<Question> GetQuestions(int? categoryId, int limit);
         public string CreateNewQuestion(Question question, string loggedInUserID);
         public void DeleteQuestion(string questionID);
         List<Question>? Search(string query);
